a7977341dc3535384d349a48fa606467b2eac93d
Tord Gunnarsli <tordsg@stud.ntnu.no>
2022-02-08 16:20:45 +0100

    FEAT header with profile and home pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { Profile } from './components/Profile';
 
 function App() {
+  const allPages = new Map<string, JSX.Element>();
+  allPages.set('home', Home());
+  allPages.set('profile', Profile())
+  const [current, setCurrent] = useState(Home());
+
+  const changeBody = (el: JSX.Element, path: string) => {
+    window.history.pushState(path, 'Nettside', `/${path}`);
+    setCurrent(el);
+  }
+
+  useEffect(() => {
+    let path = window.location.pathname.slice(1).toLowerCase();
+    let el = allPages.get(path);
+    if (el === undefined) {
+      path = 'home';
+      el = allPages.get(path)!;
+    };
+    changeBody(el, path)
+  }, [])
+
   return (
-    <div >
-    <h1>Hello world</h1>
-    
+    <div>
+      <Header changer={changeBody} allPages={allPages}/>
+      {current}
     </div>
   );
 }
