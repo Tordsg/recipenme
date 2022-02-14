b93fa614362cfd829de1043079f481d25181d5e8
Yngvil <yngvilks@stud.ntnu.no>
2022-02-14 14:14:11 +0100

    FEAT implement dropdown menu

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import NavDropdown from 'react-bootstrap/Nav'
+import Nav from 'react-bootstrap/Nav'
+import Dropdown from './Dropdown';
+
 
 export default function Navbar(){
     return(
         <div>
-        <nav>
-            <Link className = 'AppTitle' to = '/home'>
-                <h3>
-                    RecipeN´Me
-                </h3>
-            </Link>
-            <Link to = '/profile'>Profile</Link>
-        </nav>
-        <Outlet/>
+            <nav>
+                <Dropdown />
+                <Link className = 'AppTitle' to = '/home'>
+                    <h3>
+                        RecipeN´Me
+                    </h3>
+                </Link>
+                <Link to = '/profile'>Profile</Link>
+            </nav>
+            <Outlet/>
         </div>
     )
 }
