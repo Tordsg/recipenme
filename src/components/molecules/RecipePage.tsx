035e9ab436eaa9d9ed0613ab6981bede0b06c33e
Yngvil <yngvilks@stud.ntnu.no>
2022-03-25 14:48:19 +0100

    FEAT: Grids are ready for content

---
+++ src/components/molecules/RecipePage.tsx
@@ -0,0 +1,26 @@
+import { textAlign } from '@mui/system';
+import React from 'react';
+import "./RecipePage.css"
+
+export default function RecipePage(){
+
+    return(
+        <div className="wrapper">
+            <div className="title">
+                <h1>Title</h1> 
+                <h3 style={{paddingTop:'40px'}}> Tidsanslag Kategori </h3>   
+            </div>
+            <div className="photo">
+
+            </div>
+            <div className="ingredients">
+
+            </div>
+            <div className="description">
+
+            </div>
+
+        </div>
+
+    );
+}
